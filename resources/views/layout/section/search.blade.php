9df05fc4c76ad6ac5fb19ebdd52a97ca799de9b5
ducbaoframgia <phan.duc.bao@framgia.com>
2018-10-19 08:41:37 +0700

    Fix 19-10

--- resources/views/layout/section/search.blade.php
+++ resources/views/layout/section/search.blade.php
@@ -5,14 +5,17 @@
             <ul class="suggestions-list">
                 @if (count($value) > 0)
                     @foreach ($value as $book)
-                        <li class="result-entry" data-suggestion="Target 1" data-position="1" data-type="type" data-analytics-type="merchant">
+                        <li class="result-entry" data-suggestion="Target 1" data-position="1" data-type="type"
+                            data-analytics-type="merchant">
                             <a href="{{ route('books.show', $book->slug . '-' . $book->id) }}" class="result-link">
                                 <div class="media">
                                     <div class="media-left">
                                         @if ($book->medias->count() > 0)
-                                            <img src="{{ asset(config('view.image_paths.book') . $book->medias[0]->path) }}" alt="item" class="media-object" />
+                                            <img src="{{ asset(config('view.image_paths.book') . $book->medias[0]->path) }}"
+                                                 alt="item" class="media-object"/>
                                         @else
-                                            <img src="{{ asset(config('view.image_paths.item') . 'default.jpg') }}" alt="woman" class="media-object" />
+                                            <img src="{{ asset(config('view.image_paths.item') . 'default.jpg') }}"
+                                                 alt="woman" class="media-object"/>
                                         @endif
                                     </div>
                                     <div class="media-body">
@@ -35,14 +38,17 @@
     <ul class="suggestions-list">
         @if (count($users) > 0)
             @foreach ($users as $user)
-                <li class="result-entry" data-suggestion="Target 1" data-position="1" data-type="type" data-analytics-type="merchant">
+                <li class="result-entry" data-suggestion="Target 1" data-position="1" data-type="type"
+                    data-analytics-type="merchant">
                     <a href="{{ route('user', $user->id) }}" class="result-link">
                         <div class="media">
                             <div>
                                 @if ($user->avatar)
-                                    <img src="{{ asset(config('view.image_paths.user') . $user->avatar) }}" alt="item" class="media-object mg-thumbnail avatar-icon" />
+                                    <img src="{{ $user->avatar }}" alt="item"
+                                         class="media-object mg-thumbnail avatar-icon"/>
                                 @else
-                                    <img src="{{ asset(config('view.image_paths.user') . 'default.jpg') }}" alt="woman" class="media-object mg-thumbnail avatar-icon" />
+                                    <img src="{{ asset(config('view.image_paths.user') . 'default.jpg') }}" alt="woman"
+                                         class="media-object mg-thumbnail avatar-icon"/>
                                 @endif
                             </div>
                             <div class="media-body">
