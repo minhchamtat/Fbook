3a91b77c62a58e5e79cbc602332a5357d7881b39
cuong <lxc150896@gmail.com>
2019-01-29 09:02:00 +0700

    fixOption

--- resources/views/layout/slider.blade.php
+++ resources/views/layout/slider.blade.php
@@ -5,8 +5,10 @@
                 <div class="row">
                     <div class="col-md-5">
                         <div class="slider-content slider-animated-1 text-center">
-                            <h1>{{ $textBanners[0]['value'] }}</h1>
-                            <h3>{{ $textBanners[1]['value'] }}</h3>
+                            @if (isset($textBanners) && count($textBanners) >0)
+                                <h1>{{ $textBanners[0]['value'] }}</h1>
+                                <h3>{{ $textBanners[1]['value'] }}</h3>
+                            @endif
                             <a href="{{ route('books.create') }}" class="{{ Auth::check() ? '' : 'login' }}">{{ __('settings.createBook') }}</a>
                         </div>
                     </div>
@@ -16,8 +18,10 @@
         <div class="single-slider slider-h1-2 pt-215 pb-100 bg-img" style="background-image:url({{ asset(config('view.image_paths.slide') . $banners[1]['value']) }});">
             <div class="container">
                 <div class="slider-content slider-content-2 slider-animated-1">
-                    <h1>{{ $textBanners[2]['value'] }}</h1>
-                    <h2>{{ $textBanners[3]['value'] }}</h2>
+                    @if (isset($textBanners) && count($textBanners) >0)
+                        <h1>{{ $textBanners[2]['value'] }}</h1>
+                        <h2>{{ $textBanners[3]['value'] }}</h2>
+                    @endif
                     <a href="{{ route('books.create') }}" class="{{ Auth::check() ? '' : 'login' }}">{{ __('settings.createBook') }}</a>
                 </div>
             </div>
