7e127bbe18a8ec69b7bfbb0d14d15e8705343b97
cuong <lxc150896@gmail.com>
2019-03-14 16:08:48 +0700

    imgViblo

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -5,9 +5,7 @@ function bannerImg()
     try {
         $context = stream_context_create(['http' => ['header' => 'Connection: close\r\n']]);
         $url = 'https://viblo.asia/';
-        $start = config('view.start');
-        $matchLeng = config('view.maxBanner');
-        $content = file_get_contents($url, false, $context, $start, $matchLeng);
+        $content = file_get_contents($url, false, $context);
         $data = preg_match('/<div class="container px-0"><img src="(.*?)"/is', $content, $match);
         if (isset($match[1])) {
             return $match[1];
